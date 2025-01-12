8b5dac8dd9817664dd3085633a583130becbc904
Solka99 <alicjas508@gmail.com>
2025-01-12 15:47:12 +0100

    Wrzucenie modeli do osobnego pliku models.py

---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# Modele bazodanowe
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    variables = db.relationship('UserDefinedVariables', backref='user', lazy=True)
+
+class IoTDevice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(100), unique=True, nullable=False)  # Unikalny identyfikator urządzenia
+    name = db.Column(db.String(100), nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    config = db.Column(db.JSON, nullable=True)  # Przechowywanie konfiguracji urządzenia
+    registered_at = db.Column(db.DateTime, default=datetime.now)  # Data rejestracji urządzenia
+
+
+class UserDefinedVariables(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),  primary_key=True, nullable=True)
+    lower_temp_limit = db.Column(db.Integer, nullable=True)
+    higher_temp_limit = db.Column(db.Integer, nullable=True)
+
+    # Relacja do tabeli SendTimes
+    send_times = db.relationship('SendTimes', backref='variables', lazy=True)
+
+
+class SendTimes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    variables_id = db.Column(db.Integer, db.ForeignKey('user_defined_variables.user_id'), nullable=False)
+    send_time = db.Column(db.Time, nullable=False)  # Przechowuje konkretną godzinę wysyłania
